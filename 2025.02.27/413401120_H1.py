5c6598a658e971cf3f1e52a3625c8df75de36781
WatchAndyTW <andyediteroffice@gmail.com>
2025-02-27 17:16:26 +0800

    refactor: rename file

--- 2025.02.27/413401120_H1.py
+++ 2025.02.27/413401120_H1.py
@@ -1,7 +1,7 @@
 # WatchAndyTW, 2025/02/27
 
-def factor_num_generator():
+def factor_number_generator():
     num = int(input("Enter a number: "))
     print([x for x in range(1, num + 1) if num % x == 0])
 
-factor_num_generator()
+factor_number_generator()
